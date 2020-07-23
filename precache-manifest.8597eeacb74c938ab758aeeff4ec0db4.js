19f8ecedea022a18c8ce084c7a8e2388d2bc2943
padmavathi5 <nunnapadmavathi2000@gmail.com>
2020-07-23 11:45:15 +0530

    Updates

---
+++ precache-manifest.8597eeacb74c938ab758aeeff4ec0db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b458f16029405bbca54b25de335fd248",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "0ed1e4df8e1910e13f06",
+    "url": "/To-do-list/static/css/main.0fcf6fb4.chunk.css"
+  },
+  {
+    "revision": "13a3f56d21e871f4779f",
+    "url": "/To-do-list/static/js/2.d7a29489.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.d7a29489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed1e4df8e1910e13f06",
+    "url": "/To-do-list/static/js/main.9c9c70d7.chunk.js"
+  },
+  {
+    "revision": "b243709fc4e18849f9d8",
+    "url": "/To-do-list/static/js/runtime-main.a5b8843b.js"
+  }
+]);
